d246155afbc196a7b2d8f1b7583b97ddbe7b0910
ncuhome-huyue <32433710+ncuhome-huyue@users.noreply.github.com>
2017-10-01 22:20:29 +0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -5,14 +5,18 @@ import re
 dir_ = os.listdir()
 while dir_:
     a = dir_.pop()
-    p.compile(r".*\..*")
-    if p.findall(a)
-		f = open(a,'r')
-		b = bytes(f.read(),encode('utf-8'))
-		f.close()
-		f = open(a,'wb')
-		f.write(b)
-		f.close()
+    p = re.compile(r".*\.txt")
+    if p.findall(a):
+        f = open(a,'r')
+        b = f.read()
+        b = b.encode('utf-8')
+        f.close()
+        f = open(a,'wb')
+        f.write(b)
+        f.close()
     else:
-		chdir(r'.' + os.sep +a)
+        os.chdir(r"." + os.sep + a)
         dir_ = os.listdir()
+print('end')
+        
+
